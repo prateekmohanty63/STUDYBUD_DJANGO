b73a37d43049fbdd52e2d237e6823af5606e2c45
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-01-25 15:32:48 +0530

    Feat(Room) Added room and user many to many relationship and displayed comments

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,7 @@
 from http.client import HTTPResponse
 from multiprocessing import context
 from django.shortcuts import render,redirect
-from .models import Room,Topic
+from .models import Message, Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -89,7 +89,18 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    room_messages=room.message_set.all()
+    room_messages=room.message_set.all().order_by('-created')
+
+    # message = room,body,user
+    if request.method=="POST":
+        
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room',pk=room.id)
+
     context={'room':room,'room_messages':room_messages}
     return render(request,'base/room.html',context)
 
