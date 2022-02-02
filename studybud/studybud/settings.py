bcba1156c36c35474c0accb9d6a9df989ea5ed6f
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-03 00:16:28 +0530

    Completed frontend || Added rest frameWork (rooms,room)

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+
+    # REST FRAME WORK
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
